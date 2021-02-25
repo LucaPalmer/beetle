eb6f5f903ebb316b65016af5054b55e14714e995
Luca <66808669+senkuluca@users.noreply.github.com>
2021-02-25 15:00:34 +0000

    Add files via upload
    body, head and head features function complete

--- btul.py
+++ btul.py
@@ -7,16 +7,47 @@ Created on Mon Feb 22 16:31:12 2021
 """
 import random
 
-beetle = {1: "body", 2:"head", 3: "antennae", 4: "eye", 5: "mouth", 6: "leg"}
+dice = (1, 2, 3, 4, 5, 6)
+
+dice_order = {1: "body", 2:"head", 3: "antennae", 4: "eye", 5: "mouth", 6: "leg"}
 
 player = []
 
-dice = (1, 2, 3, 4, 5, 6)
+def body():
+    if result in dice_order:
+        if result == 1:
+            player.append(dice_order[result])
+            print("Dice: ", result)
+            print("Beetle: ", player)
+        else:
+            print("Dice: ", result)
+            print("You didn't roll one, try again.")
 
-def roll():
-    result = random.choice(dice)
-    for i in beetle:
-        if i == result:
-            player.append(beetle[i])
-            print(result)
-            print("Beetle: ", player)  
+
+def head():
+    if dice_order[1] in player:
+        if result in dice_order:
+            if result == 2 or result == 6:
+                player.append(dice_order[result])
+            print("Dice: ", result)
+            print("Beetle: ", player)
+        else:
+            print("Dice: ", result)
+            print("You didn't roll two, try again.")
+
+def head_features():
+    if dice_order[1 and 2] in player:
+        if result in dice_order:
+            if result == 3 or result == 4 or result == 5 or result == 6:
+                player.append(dice_order[result])
+            print("Dice: ", result)
+            print("Beetle: ", player)
+        else:
+            print("Dice: ", result)
+            print("You didn't roll one, try again.")
+
+
+def beetle():
+   rotation = {1: body, 2: head, 3: head_features}
+   
+   
